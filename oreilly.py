293d419d8f6b80d18b5e44bad52b31037176f44a
adminprivileges <caldwellthaddeus@orotonmail.com>
2022-05-07 10:22:03 -0400

    intial commit, untested but login in place

---
+++ oreilly.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+import time, oreilly_vars
+
+
+#TODO: input python scheduler logic so that the script keeps looking for email
+class oreilly:
+    def open_account(self):
+        #This prevents me from having to dwnload the gecko self.driver
+        self.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        #im prolly gonna have to add vars later
+        vars = {}
+        #open the oreilly site
+        self.driver.get("https://www.oreilly.com/member/login/")
+        self.driver.set_window_size(894, 1430)
+        #click the username form
+        self.driver.find_element(By.CSS_SELECTOR, ".orm-Input-labelTxt").click()
+        #input username
+        self.driver.find_element(By.ID, "input-yrseibl2vce").send_keys(oreilly_vars.user)
+        self.driver.find_element(By.CSS_SELECTOR, ".orm-Button-btnContentWrap").click()
+        element = self.driver.find_element(By.CSS_SELECTOR, ".orm-Input-labelTxt")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).click_and_hold().perform()
+        element = self.driver.find_element(By.ID, "input-1b7bwfrw657")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).release().perform()
+        #click password form
+        self.driver.find_element(By.CSS_SELECTOR, ".orm-Input-inputLabel").click()
+        #enter password
+        self.driver.find_element(By.ID, "input-1b7bwfrw657").send_keys(oreilly_vars.password)
+        self.driver.find_element(By.CSS_SELECTOR, ".orm-Button-btnContentWrap").click()
+        self.driver.quit()
+
+Oreilly = oreilly()
+Oreilly()
