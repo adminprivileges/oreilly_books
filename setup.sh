f47acbf928a716243b0f2865538c6350367fbfde
adminprivileges <caldwellthaddeus@orotonmail.com>
2022-06-11 12:27:22 -0400

    fixed setup script

--- setup.sh
+++ setup.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 #install Python Dependancies
-pip install -r requirements.txt 
 pip install virtualenv
+
 # Create user/pass variables
 read -p "Enter your Oreilly username: " OR_USER
 read -p "Enter your Oreilly password: " OR_PASS
-echo "user = $OR_USER
-password = $OR_PASS
+echo "user = "$OR_USER"
+password = "$OR_PASS"
 " | tee ./oreilly_vars.py
 
 #creates a virtual environemnt
 python3 -m venv ./venv
+source ./venv/bin/activate
+pip install -r requirements.txt 
 
 #adding chronjob to run weekly
 crontab -l | { cat; echo "@weekly `pwd`/venv/bin/python `pwd`/oreilly.py"; } | crontab -
