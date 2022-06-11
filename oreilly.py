cc7934f936bc1041b27b5937e36c91590f051872
adminprivileges <caldwellthaddeus@orotonmail.com>
2022-06-11 12:45:33 -0400

    changed exec_path to service

--- oreilly.py
+++ oreilly.py
@@ -14,7 +14,7 @@ class oreilly:
         options = Options()
         options.headless = True
         #This prevents me from having to dwnload the gecko self.driver
-        self.driver = webdriver.Firefox(options=options, executable_path=GeckoDriverManager().install())
+        self.driver = webdriver.Firefox(options=options, serive=Service(GeckoDriverManager().install()))
         #im prolly gonna have to add vars later
         vars = {}
         #open the oreilly site
